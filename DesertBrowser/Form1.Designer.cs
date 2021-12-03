fdaa389a4c4f44d4be4c11acf6725433b67e858d
Scott <80927112+scottlooker@users.noreply.github.com>
2021-12-03 23:33:06 +0000

    Add files via upload

---
+++ DesertBrowser/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace DesertBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BrowserPanel = new System.Windows.Forms.Panel();
+            this.UrlBox = new System.Windows.Forms.TextBox();
+            this.BrowserGoButton = new System.Windows.Forms.Button();
+            this.Copyright = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BrowserPanel
+            // 
+            this.BrowserPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.BrowserPanel.Location = new System.Drawing.Point(0, 65);
+            this.BrowserPanel.Name = "BrowserPanel";
+            this.BrowserPanel.Size = new System.Drawing.Size(800, 385);
+            this.BrowserPanel.TabIndex = 0;
+            // 
+            // UrlBox
+            // 
+            this.UrlBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.UrlBox.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.UrlBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.UrlBox.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UrlBox.ForeColor = System.Drawing.Color.White;
+            this.UrlBox.Location = new System.Drawing.Point(12, 11);
+            this.UrlBox.Name = "UrlBox";
+            this.UrlBox.Size = new System.Drawing.Size(694, 20);
+            this.UrlBox.TabIndex = 1;
+            this.UrlBox.TextChanged += new System.EventHandler(this.UrlBox_TextChanged);
+            // 
+            // BrowserGoButton
+            // 
+            this.BrowserGoButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BrowserGoButton.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BrowserGoButton.Location = new System.Drawing.Point(712, 11);
+            this.BrowserGoButton.Name = "BrowserGoButton";
+            this.BrowserGoButton.Size = new System.Drawing.Size(76, 20);
+            this.BrowserGoButton.TabIndex = 2;
+            this.BrowserGoButton.Text = "Go";
+            this.BrowserGoButton.UseVisualStyleBackColor = true;
+            this.BrowserGoButton.Click += new System.EventHandler(this.BrowserGoButton_click);
+            // 
+            // Copyright
+            // 
+            this.Copyright.AutoSize = true;
+            this.Copyright.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Copyright.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Copyright.Location = new System.Drawing.Point(8, 34);
+            this.Copyright.Name = "Copyright";
+            this.Copyright.Size = new System.Drawing.Size(333, 19);
+            this.Copyright.TabIndex = 3;
+            this.Copyright.Text = "Desert Browser (c) 2021 Scott Looker";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Copyright);
+            this.Controls.Add(this.BrowserGoButton);
+            this.Controls.Add(this.UrlBox);
+            this.Controls.Add(this.BrowserPanel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Desert Browser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel BrowserPanel;
+        private System.Windows.Forms.TextBox UrlBox;
+        private System.Windows.Forms.Button BrowserGoButton;
+        private System.Windows.Forms.Label Copyright;
+    }
+}
+
---
+++ DesertBrowser/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace DesertBrowser
+{
+    public partial class Form1 : Form
+    {
+        // Variables
+        private ChromiumWebBrowser chromeBrowser;
+
+        // Init methods
+        private void InitializeChromium() 
+        {
+            CefSettings settings = new CefSettings();
+            // Initialize cef with the provided settings
+            Cef.Initialize(settings);
+            // Create a browser component
+            chromeBrowser = new ChromiumWebBrowser("https://www.duckduckgo.com");
+            // Add it to the form and fill it to the form window.
+            BrowserPanel.Controls.Add(chromeBrowser);
+            chromeBrowser.Dock = DockStyle.Fill;
+        }
+
+        // Constructor
+        public Form1() 
+        {
+            InitializeComponent();
+            InitializeChromium();
+        }
+
+        // Form methods
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) => Cef.Shutdown();
+
+        private void BrowserGoButton_click(object sender, EventArgs e) => chromeBrowser.Load(UrlBox.Text);
+
+        private void UrlBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
